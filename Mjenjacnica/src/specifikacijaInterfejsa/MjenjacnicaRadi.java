37b8b14612cbbe0a8cc760b5ab9ba213f4b0eb3f
bojanabakic <bojanabakic96@gmail.com>
2017-03-29 16:44:44 +0200

    Implementirane metode klase MjenjacnicaRadi.

--- Mjenjacnica/src/specifikacijaInterfejsa/MjenjacnicaRadi.java
+++ Mjenjacnica/src/specifikacijaInterfejsa/MjenjacnicaRadi.java
@@ -1,28 +1,45 @@
 package specifikacijaInterfejsa;
 
 import java.util.GregorianCalendar;
+import java.util.LinkedList;
 
 import valuta.Valuta;
 
 public class MjenjacnicaRadi implements KursMetode {
-
+	LinkedList<Valuta> kursnaLista = new LinkedList<Valuta>();
+	
 	@Override
 	public void dodajKurs(String naziv, String skraceniNaziv, GregorianCalendar datum, double kupovniKurs,
-			double prodajniKurs, double srednjiKurs) {
-		// TODO Auto-generated method stub
-
+			double prodajniKurs, double srednjiKurs) throws Exception {
+		Valuta novaValuta = new Valuta();
+		novaValuta.setNaziv(naziv);
+		novaValuta.setSkraceniNaziv(skraceniNaziv);
+		novaValuta.setDatumUnosaKursa(datum);
+		novaValuta.setKupovniKurs(kupovniKurs);
+		novaValuta.setProdajniKurs(prodajniKurs);
+		novaValuta.setSrednjiKurs(srednjiKurs);
+		
+		if(!kursnaLista.contains(novaValuta)){
+			kursnaLista.add(novaValuta);
+		} else throw new Exception("Kurs je vec unijet.");
 	}
 
 	@Override
 	public void obrisiKurs(String naziv, GregorianCalendar datum) {
-		// TODO Auto-generated method stub
-
+		for (int i = 0; i < kursnaLista.size(); i++) {
+			if(kursnaLista.get(i).getNaziv().equals(naziv) && kursnaLista.get(i).getDatumUnosaKursa() == datum){
+				kursnaLista.remove(i);
+			}
+		}
 	}
 
 	@Override
 	public Valuta vratiKurs(String naziv, GregorianCalendar datum) {
-		// TODO Auto-generated method stub
+		for (int i = 0; i < kursnaLista.size(); i++) {
+			if(kursnaLista.get(i).getNaziv().equals(naziv) && kursnaLista.get(i).getDatumUnosaKursa() == datum){
+				return kursnaLista.get(i);
+			}else throw new RuntimeException();
+		}
 		return null;
 	}
-
 }
